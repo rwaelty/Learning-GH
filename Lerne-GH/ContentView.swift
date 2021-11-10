68d3eabd84278448e92095c47a4606aa8c431021
rwaelty <rwaelty@bluewin.ch>
2021-11-10 17:37:01 +0100

    Initial Commit

---
+++ Lerne-GH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lerne-GH
+//
+//  Created by Rolf Waelty on 10.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lerne-GH/Lerne_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lerne_GHApp.swift
+//  Lerne-GH
+//
+//  Created by Rolf Waelty on 10.11.21.
+//
+
+import SwiftUI
+
+@main
+struct Lerne_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
